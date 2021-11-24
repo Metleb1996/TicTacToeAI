60d9f298816e58a3d97b0fbee6ee2467d61b3912
Metleb1996 <metlebrustemov1996@gmail.com>
2021-11-24 14:58:00 +0400

    24.11.2021

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from tictactoe import TicTacToe
 from dummy import Dummy
 from human import Human
+from agent import Agent
 
 game = TicTacToe(first='O')
 dummy = Dummy(name='O')
 human = Dummy(name='X')
+agent = Agent(name="X")
 
 def print_board(board):
     print("\n{} {} {}\n{} {} {}\n{} {} {}\n______\n".format(*board))
 
 if __name__ == "__main__":
-    for i in range(9):
+    agent.train(first='O', dum=dummy, count=1000000)
+    """ for i in range(9):
         d, game = dummy.playStep(game=game); print(d)
         b = game.getBoard()
         print_board(b)
@@ -25,5 +28,5 @@ if __name__ == "__main__":
         if game.isBoardFilled():
             print("Hec-hece")
             break
-        
-        
+         """
+    
